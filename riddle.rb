64ac96546b060cf161d52df21ed4b0ac438a6ae7
Awlter <wxfwalter@gmail.com>
2018-03-15 21:21:42 +0800

    finish my first framework

---
+++ riddle.rb
@@ -0,0 +1,16 @@
+class Riddle
+
+  private
+  
+  def erb(filename, local = {})
+    b = binding
+    message = local[:message]
+    content = File.read("views/#{filename}.erb")
+    ERB.new(content).result(b)
+  end
+
+  def response(status, headers, body = '')
+    body = [yield] if block_given?
+    [status, headers, body]
+  end
+end
